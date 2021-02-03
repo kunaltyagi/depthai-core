025ef2b77ffc95f5b9a535e5d85f25e207a31b51
SzabolcsGergely <szabi@luxonis.com>
2021-02-03 09:01:25 +0200

    Update FW with ppenc fixes

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")  
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "5b6abbe617695d431568ab01fa27e3fa7468da69")            
+set(DEPTHAI_DEVICE_SIDE_COMMIT "50957d0da51dc4fd3816e186d775b2da28ebd0af")            
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
